526bca0c4e9d3437be950ed604c1ac3298e9ccd1
Brant Faircloth <brant.faircloth@gmail.com>
2011-06-23 23:28:23 -0700

    modularizing code

--- get_fake_velvet_contigs_from_genomes.py
+++ get_fake_velvet_contigs_from_genomes.py
@@ -12,7 +12,6 @@ Copyright 2011 Brant C. Faircloth. All rights reserved.
 import os
 import sys
 import glob
-import sqlite3
 import argparse
 import textwrap
 import bx.seq.twobit
@@ -41,7 +40,7 @@ def get_args():
     parser.add_argument('--dupefile', help='The path to a lastz file of lastz-against-self results')
     return parser.parse_args()
 
-def run_checks(k, v, probes):
+def run_checks(k, v, probes, verbose = True):
     try:
         # make sure we have as many matches as probes
         assert len(v) == probes[k]              # more matches than expected
@@ -49,9 +48,9 @@ def run_checks(k, v, probes):
         assert len(set([i[1] for i in v])) == 1 # multiple match directions
         return True
     except AssertionError:
-        print "Probe: ", k
+        if verbose:print "Probe: ", k
         result_string = ["{0}:{1}-{2}|{3}".format(i[0], i[2], i[3], i[1]) for i in v]
-        print "\t\tExpected hits: {0}\n\t\tObserved Hits: {1}\n\t\t\t{2}".format(probes[k], len(v), '\n\t\t\t'.join(result_string))
+        if verbose:print "\t\tExpected hits: {0}\n\t\tObserved Hits: {1}\n\t\t\t{2}".format(probes[k], len(v), '\n\t\t\t'.join(result_string))
         return False
 
 def get_matches(lastz_file, splitchar, components):
