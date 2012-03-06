2b6c41bd51bbe0f6b6a7ca52083642abeb3851f6
Brant Faircloth <brant.faircloth@gmail.com>
2012-03-05 20:40:15 -0800

    compute raw base counts across all alignments

--- bin/align/get_counts_of_taxa_in_align.py
+++ bin/align/get_counts_of_taxa_in_align.py
@@ -22,7 +22,7 @@ import argparse
 from Bio import AlignIO
 from phyluce.helpers import is_dir
 
-from collections import Counter
+from collections import Counter, defaultdict
 
 
 def get_args():
@@ -30,6 +30,8 @@ def get_args():
     parser.add_argument('nexus', help='The directory containing the nexus files', type=is_dir)
     parser.add_argument('--min-taxa', help='''The minimum number of taxa to
             count''', type=int)
+    parser.add_argument('--count-bases', dest='count_bases', help='''The minimum number of taxa to
+            count''', action='store_true', default=False)
     return parser.parse_args()
 
 def get_files(input_dir):
@@ -40,8 +42,13 @@ def main():
     # iterate through all the files to determine the longest alignment
     files = get_files(args.nexus)
     counts = []
+    bases = defaultdict(list)
     for f in files:
         aln = AlignIO.read(f, 'nexus')
+        if args.count_bases:
+            for col in xrange(len(aln[0])):
+                for base in aln[:,col]:
+                    bases[base].append(1)
         if args.min_taxa:
             if len(aln) >= args.min_taxa:
                 counts.append(len(aln))
@@ -49,10 +56,12 @@ def main():
              counts.append(len(aln))
     print "Average: {}".format(sum(counts)/float(len(counts)))
     ci = 1.96 * numpy.std(numpy.array(counts), ddof=1)/numpy.sqrt(len(counts))
+    base_sum = {base:sum(bases[base]) for base in bases}
     print "95 CI: {}".format(ci)
     print "(min): {}".format(min(counts))
     print "(max): {}".format(max(counts))
-    print "Count: {}".format(dict(Counter(counts)))
+    print "Count {{Num taxa:alns with Num taxa}}: {}".format(dict(Counter(counts)))
+    print "Bases: {}".format(base_sum)
 
 if __name__ == '__main__':
     main()
