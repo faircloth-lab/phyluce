c51322875d4cf023536043483d1fa7a2c4e73790
Brant Faircloth <brant.faircloth@gmail.com>
2012-03-08 11:35:53 -0800

    PEP8 cleanup

--- bin/align/seqcap_align.py
+++ bin/align/seqcap_align.py
@@ -6,16 +6,16 @@ seqcap_align.py
 Created by Nicholas Crawford and Brant C. Faircloth
 Copyright (c) 2010 Nicholas Crawford and Brant C. Faircloth. All rights reserved.
 
-This script aligns fasta sequence groups on a per locus basis (where the locus 
-name is in fasta header).  It takes a fasta file of reads, in arbitrary order, 
-groups reads by locus and uses MUSCLE (http://www.drive5.com/muscle/) to do 
-align reads by locus.  We use the Biopython Alignment class to hold and 
-output reads in various formats (fasta, nexus, etc).  We've also implemented 
-a class (ConcatenatedAlignment) which concatenates alignments by locus, and 
+This script aligns fasta sequence groups on a per locus basis (where the locus
+name is in fasta header).  It takes a fasta file of reads, in arbitrary order,
+groups reads by locus and uses MUSCLE (http://www.drive5.com/muscle/) to do
+align reads by locus.  We use the Biopython Alignment class to hold and
+output reads in various formats (fasta, nexus, etc).  We've also implemented
+a class (ConcatenatedAlignment) which concatenates alignments by locus, and
 outputs one large alignment in various formats.
 
-We've also implemented options to trim aligned reads at specified distances 
-from an internal probe sequence and also remove the specified sequence 
+We've also implemented options to trim aligned reads at specified distances
+from an internal probe sequence and also remove the specified sequence
 from within reads.
 
 python seqcapAlign.py --input=all.fa --output=output/ \
@@ -37,6 +37,7 @@ from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import IUPAC, Gapped
 from Bio.Align.Generic import Alignment
 
+
 class ConcatenatedAlignment(Alignment):
     '''A child class of the biopython alignment class, created so that we could
     add the concat method to concatenate multiple alignments'''
@@ -57,10 +58,11 @@ class ConcatenatedAlignment(Alignment):
                     # this assumes that we will be using id as
                     # the joining attribute...
                     if seq.id.split('_')[-1] == record.id:
-                        c_seq       = SeqRecord(record.seq + seq.seq)
-                        c_seq.name  = record.id
-                        c_seq.id    = record.id
+                        c_seq = SeqRecord(record.seq + seq.seq)
+                        c_seq.name = record.id
+                        c_seq.id = record.id
                         self._records[pos] = c_seq
+
 
 class Locus(list):
     '''a subclass of list to hold a group of sequence reads on a per
@@ -69,9 +71,9 @@ class Locus(list):
         list.__init__(self)
         if arg:
             self.append(arg[0])
-        self.contents   = ''
-        self.formatted  = False
-        self.tempFile   = None
+        self.contents = ''
+        self.formatted = False
+        self.tempFile = None
 
     def formatSequences(self):
         '''create the contents of a fasta file on a per locus basis'''
@@ -90,80 +92,65 @@ class Locus(list):
     #    '''remove the tempfile'''
     #    os.remove(self.tempFile) 
 
+
 def interfaceWarnings(p, message):
     '''generic warning function for interface options'''
-    
     print message
     p.print_help()
     sys.exit(2)
 
+
 def interface():
     '''Get the starting parameters from a configuration file'''
     usage = "usage: %prog [options]"
-
     p = optparse.OptionParser(usage)
-
-    p.add_option('--input', dest = 'path', action='store', \
-        type='string', default = None, \
+    p.add_option('--input', dest='path', action='store', \
+        type='string', default=None, \
         help='The path to the input file.', metavar='FILE')
-        
-    p.add_option('--output', dest = 'output', action='store', \
-        type='string', default = None, \
+    p.add_option('--output', dest='output', action='store', \
+        type='string', default=None, \
         help='The path to the output directory.', metavar='FILE')
-    
-    p.add_option('--format', dest = 'format', action='store', \
-        type='string', default = 'nexus', \
+    p.add_option('--format', dest='format', action='store', \
+        type='string', default='nexus', \
         help='Output format:  clustal, emboss, fasta, nexus, phylip,'\
             +'stockholm')
-        
-    p.add_option('--concatenate', dest = 'concatenate', action='store_true', \
-        default = False, help='Concatenate alignments by locus')
-    
-    p.add_option('--probe-file', dest = 'probefile', action='store', \
-        type='string', default = None, \
+    p.add_option('--concatenate', dest='concatenate', action='store_true', \
+        default=False, help='Concatenate alignments by locus')
+    p.add_option('--probe-file', dest='probefile', action='store', \
+        type='string', default=None, \
         help='Path to a fasta file containing the probe sequences.', \
-        metavar='FILE') 
-    
-    p.add_option('--trim-flank', dest = 'trimflank', action='store', \
-        type='int', default = None, \
+        metavar='FILE')
+    p.add_option('--trim-flank', dest='trimflank', action='store', \
+        type='int', default=None, \
         help='The length to trim the flank.')
-    
-    p.add_option('--species', dest = 'species', action='store', \
-        type='int', default = None, \
-        help='Number of species expected in each alignment.')  
-    
-    p.add_option('--no-ambiguous', dest = 'no_ambiguous', action='store_true', \
-        default = False, help='Don\'t allow reads with ambiguous (N) bases')
-    
-    p.add_option('--trim-ambiguous', dest = 'trim_ambiguous', action='store_true', \
-        default = False, help='Allow reads with ambiguous (N) bases, but trim'\
-            +' them out following alignment')
-    
-    p.add_option('--trim-running', dest = 'trimrunning', action='store_true', \
-        default = False, help='Trim alignments using a running average')
-    
-    p.add_option('--remove-probe', dest = 'removeprobe', action='store_true', \
-        default = False, help='Remove the probe sequence from the alignment.')
-    
-    p.add_option('--keep-probe', dest = 'keepprobe', action='store_true', \
-        default = False, help='Add the probe sequence to the alignment.')
-    
-    p.add_option('--multiprocessing', dest = 'multiprocessing', action='store_true', \
-        default = False, help='Use mutiple cores/processors.')
-    
-    p.add_option('--processors', dest = 'processors', action='store', \
-        type='int', default = multiprocessing.cpu_count()-2, \
+    p.add_option('--species', dest='species', action='store', \
+        type='int', default=None, \
+        help='Number of species expected in each alignment.')
+    p.add_option('--no-ambiguous', dest='no_ambiguous', action='store_true', \
+        default=False, help='Don\'t allow reads with ambiguous (N) bases')
+    p.add_option('--trim-ambiguous', dest='trim_ambiguous', action='store_true', \
+        default=False, help='Allow reads with ambiguous (N) bases, but trim' + \
+                ' them out following alignment')
+    p.add_option('--trim-running', dest='trimrunning', action='store_true', \
+        default=False, help='Trim alignments using a running average')
+    p.add_option('--remove-probe', dest='removeprobe', action='store_true', \
+        default=False, help='Remove the probe sequence from the alignment.')
+    p.add_option('--keep-probe', dest='keepprobe', action='store_true', \
+        default=False, help='Add the probe sequence to the alignment.')
+    p.add_option('--multiprocessing', dest='multiprocessing', action='store_true', \
+        default=False, help='Use mutiple cores/processors.')
+    p.add_option('--processors', dest='processors', action='store', \
+        type='int', default=multiprocessing.cpu_count() - 2, \
         help='Number of cores to use.')
 
-    p.add_option('--notstrict', dest = 'notstrict', action='store_true', \
-        default = False, help='Allow alignments containing not all species.')
+    p.add_option('--notstrict', dest='notstrict', action='store_true', \
+        default=False, help='Allow alignments containing not all species.')
 
-    p.add_option('--faircloth', dest = 'faircloth', action='store_true', \
-        default = False, help='Take faircloth+stephens probe names')
+    p.add_option('--faircloth', dest='faircloth', action='store_true', \
+        default=False, help='Take faircloth+stephens probe names')
 
-    
-    p.add_option('--verbose', dest = 'verbose', action='store_true', \
-        default = False, help='List locus names while processing.') 
+    p.add_option('--verbose', dest='verbose', action='store_true', \
+        default=False, help='List locus names while processing.') 
     
     (options,arg) = p.parse_args()
     if not options.species:
@@ -188,10 +175,12 @@ def interface():
         interfaceWarnings(p, "This is an unsupported output format")
     options.format_extension = formats[options.format]
     return options, arg
-    
+
+
 def outPFilename(path, fnn, extension):
     '''generalize the formatting of output filenames'''
     return (os.path.join(path, fnn)) + extension
+
 
 def multiAlign(input, output):
     for locus_name, sequences, probe, options in iter(input.get, 'STOP'):
@@ -199,14 +188,15 @@ def multiAlign(input, output):
         output.put(results)
     return
 
+
 def singleAlign(locus_name, sequences, probe, options):
     #pdb.set_trace()
     if options.verbose:
         print '\tLocus:  %s' % locus_name
     if options.trimflank or options.removeprobe:
         # give the probe a generic name so we can easily find it later
-        probe.name  = 'probe'
-        probe.id    = 'probe'
+        probe.name = 'probe'
+        probe.id = 'probe'
         probe.description = 'probe'
         # add the probe sequence to the locus
         sequences.append(probe)
@@ -214,14 +204,14 @@ def singleAlign(locus_name, sequences, probe, options):
         sequences.formatSequences()
         # create a tempfile to feed to muscle and feed it
         sequences.createTempFile()
-        muscle       = phyluce.muscle.Align(sequences.tempFile)
+        muscle = phyluce.muscle.Align(sequences.tempFile)
         muscle.run_alignment(consensus=False)
         muscle.get_probe_location()
-        muscle.trim_alignment(method='trim', probe = 'remove')
+        muscle.trim_alignment(method='trim', probe='remove')
     elif options.keepprobe:
         # give the probe a generic name so we can easily find it later
-        probe.name  = 'probe'
-        probe.id    = 'probe'
+        probe.name = 'probe'
+        probe.id = 'probe'
         probe.description = 'probe'
         # add the probe sequence to the locus
         sequences.append(probe)
@@ -229,21 +219,21 @@ def singleAlign(locus_name, sequences, probe, options):
         sequences.formatSequences()
         # create a tempfile to feed to muscle and feed it
         sequences.createTempFile()
-        muscle       = phyluce.muscle.Align(sequences.tempFile)
+        muscle = phyluce.muscle.Align(sequences.tempFile)
         muscle.run_alignment(consensus=False)
-        muscle.trim_alignment(method='running-probe', window_size = 20, threshold = 0.5)
+        muscle.trim_alignment(method='running-probe', window_size=20, threshold=0.5)
     elif options.trimrunning:
         #print sequences
         sequences.formatSequences()
         sequences.createTempFile()
-        muscle       = phyluce.muscle.Align(sequences.tempFile)
+        muscle = phyluce.muscle.Align(sequences.tempFile)
         muscle.run_alignment(consensus=False)
-        muscle.trim_alignment(method='running', window_size = 20, threshold = 0.5)
+        muscle.trim_alignment(method='running', window_size=20, threshold=0.5)
     else:
         #print sequences
         sequences.formatSequences()
         sequences.createTempFile()
-        muscle       = phyluce.muscle.Align(sequences.tempFile)
+        muscle = phyluce.muscle.Align(sequences.tempFile)
         muscle.run_alignment(consensus=False)
         muscle.trim_alignment(method='notrim')
     if options.trim_ambiguous:
@@ -254,8 +244,8 @@ def singleAlign(locus_name, sequences, probe, options):
 
 def q_runner(n_procs, loci, probes, options, function, *args):
     '''generic function used to start worker processes'''
-    task_queue      = multiprocessing.Queue()
-    results_queue   = multiprocessing.JoinableQueue()
+    task_queue = multiprocessing.Queue()
+    results_queue = multiprocessing.JoinableQueue()
     if args:
         arguments = (task_queue, results_queue,) + args
     else:
@@ -270,7 +260,7 @@ def q_runner(n_procs, loci, probes, options, function, *args):
         else:
             task_queue.put([locus, loci[locus], None, options])
     for _ in range(n_procs):
-        p = multiprocessing.Process(target = function, args = arguments).start()
+        p = multiprocessing.Process(target=function, args=arguments).start()
         #print 'Starting %s' % function
     for _ in range(len(loci)):
         # indicated done results processing
@@ -287,16 +277,18 @@ def q_runner(n_procs, loci, probes, options, function, *args):
     results_queue.close()
     return results
 
+
 def makeOutPutDir(output):
     """create directory to store output"""
     if os.path.exists(output) == True:
         overwrite = raw_input('Path exists, overwrite? [Y/n]:')
-        if overwrite.lower() == 'y' or 'yes' :
+        if overwrite.lower() == 'y' or 'yes':
             shutil.rmtree(output)
             os.mkdir(output)
         else:
             pass
     return output
+
 
 def ambiguousBaseChecker(loci, locus, record):
     if not 'N' in record.seq:
@@ -305,12 +297,14 @@ def ambiguousBaseChecker(loci, locus, record):
         print 'Skipping {0} because it contains ambiguous bases'.format(record.id)
     return loci       
 
+
 def buildLocusDict(loci, locus, record):
     if locus not in loci.keys():
         loci[locus] = Locus(record)
     else:
         loci[locus].append(record)
     return loci
+
 
 def main():
     options, arg = interface()
@@ -320,11 +314,11 @@ def main():
     else:
         alignmentBlocks = None
     if not options.output:
-        options.output = options.path 
+        options.output = options.path
     if options.trimflank or options.removeprobe or options.keepprobe:
         # create a dict of probe sequences
         #TODO: clean this up a bit
-        probes = SeqIO.to_dict(SeqIO.parse(open(options.probefile,'rU'), 'fasta'))
+        probes = SeqIO.to_dict(SeqIO.parse(open(options.probefile, 'rU'), 'fasta'))
     else:
         probes = None
     # create a holder for all loci
@@ -341,7 +335,7 @@ def main():
         print 'Removing ALL sequences with ambiguous bases...'
     else:
         print 'NOT removing sequences with ambiguous bases...'
-    for record in SeqIO.parse(open(options.path,'rU'), 'fasta'):
+    for record in SeqIO.parse(open(options.path, 'rU'), 'fasta'):
         #pdb.set_trace()
         if not options.faircloth:
             locus = record.description.split('|')[1]
@@ -386,7 +380,7 @@ def main():
         if alignment and alignment.get_alignment_length() > 50:
             # renamed from nexus to (generic) outp because function is generic now...
             print outPFilename(options.output, locus, options.format_extension)
-            outp = open(outPFilename(options.output, locus, options.format_extension),'w')
+            outp = open(outPFilename(options.output, locus, options.format_extension), 'w')
             #
             # NOTE:  this is a stop-gap measure to deal with different probe names
             #
@@ -405,7 +399,7 @@ def main():
             else:
                 print '\tLocus %s not successfully aligned due to trimming errors - skipped from writing' % locus
                 pass
-        outp = open(outPFilename(options.output, 'concat', options.format_extension),'w')
+        outp = open(outPFilename(options.output, 'concat', options.format_extension), 'w')
         try:
             outp.write(concatenatedAlignment.format(options.format))
         except:
