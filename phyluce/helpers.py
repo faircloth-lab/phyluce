28ea3a9a2a99172e2c42b8366e582a425acf12e9
Brant Faircloth <brant.faircloth@gmail.com>
2012-03-08 17:11:10 -0800

    add "which" to helpers

--- phyluce/helpers.py
+++ phyluce/helpers.py
@@ -145,3 +145,20 @@ def get_xml_data(xml, prnt = False):
         else:
             snps[rsid] = dbsnp._make(metadata)
     return snps
+
+def which(program):
+    """ from http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python"""
+    def is_exe(fpath):
+        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
