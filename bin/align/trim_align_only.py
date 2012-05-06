b18423cd5ce3ed061e41ca8a67debd896a5bcfa5
Brant Faircloth <brant.faircloth@gmail.com>
2012-05-06 16:22:26 -0700

    add code using GenericAlign to trim already-aligned sequences

---
+++ bin/align/trim_align_only.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+File: trim_align_only.py
+Author: Brant Faircloth
+
+Created by Brant Faircloth on 06 May 2012 14:05 PDT (-0700)
+Copyright (c) 2012 Brant C. Faircloth. All rights reserved.
+
+Description: 
+
+"""
+
+import os
+import sys
+import glob
+import argparse
+import multiprocessing
+from phyluce.helpers import is_dir, FullPaths
+from phyluce.generic_align import GenericAlign
+
+import pdb
+
+
+def get_args():
+    """Get arguments from CLI"""
+    parser = argparse.ArgumentParser(
+            description="""Use the PHYLUCE trimming algorithm to trim existing alignments""")
+    parser.add_argument(
+            "input",
+            action=FullPaths,
+            type=is_dir,
+            help="""The directory containing alignments"""
+        )
+    parser.add_argument(
+            "output",
+            action=FullPaths,
+            type=is_dir,
+            help="""The directory to contain trimmed alignments"""
+        )
+    parser.add_argument(
+            "--input-format",
+            dest="input_format",
+            choices=['fasta', 'nexus', 'phylip', 'clustal', 'emboss', 'stockholm'],
+            default='fasta',
+            help="""The input alignment format""",
+        )
+    parser.add_argument(
+            "--output-format",
+            dest="output_format",
+            choices=['fasta', 'nexus', 'phylip', 'clustal', 'emboss', 'stockholm'],
+            default='fasta',
+            help="""The output alignment format""",
+        )
+    parser.add_argument(
+            "--window",
+            type=int,
+            default=20,
+            help="Sliding window size for trimming"
+        )
+    parser.add_argument(
+            "--threshold",
+            type=float,
+            default=0.5,
+            help="Threshold cutoff for trimming"
+        )
+    parser.add_argument(
+            "--proportion",
+            type=float,
+            default=0.3,
+            help="Proportional removal of gaps"
+        )
+    parser.add_argument(
+            "--multiprocessing",
+            action="store_true",
+            default=False,
+            help="""Use multiprocessing""",
+            )
+    return parser.parse_args()
+
+
+def get_file_extensions(ftype):
+    ext = {
+        'fasta': ('.fasta', '.fsa', '.aln', '.fa'),
+        'nexus': ('.nexus', '.nex'),
+        'phylip': ('.phylip', '.phy'),
+        'clustal': ('.clustal', '.clw'),
+        'emboss': ('.emboss'),
+        'stockholm': ('.stockholm')
+    }
+    return ext[ftype]
+
+
+def get_and_trim_alignments(params):
+    trimming_params, align_file = params
+    input_format, window, threshold, proportion = trimming_params
+    #pdb.set_trace()
+    name = os.path.basename(os.path.splitext(align_file)[0])
+    aln = GenericAlign(align_file)
+    # call private method to read alignment into alignment object
+    try:
+        aln._read(input_format)
+        # dont return consensus
+        aln.trim_alignment(
+                method='running',
+                window_size=window,
+                threshold=threshold,
+                proportion=proportion,
+                consensus=False
+            )
+        sys.stdout.write(".")
+        sys.stdout.flush()
+        return (name, aln)
+    except ValueError, e:
+        if e.message == 'No records found in handle':
+            return (name, False)
+        else:
+            raise ValueError('Something is wrong with alignment {0}'.format(name))
+
+
+def write_alignments_to_outdir(outdir, alignments, format):
+    print '\nWriting output files...'
+    for tup in alignments:
+        locus, aln = tup
+        if aln:
+            outname = "{}{}".format(
+                    os.path.join(outdir, locus),
+                    get_file_extensions(format)[0]
+                )
+            outf = open(outname, 'w')
+            outf.write(aln.trimmed_alignment.format(format))
+            outf.close()
+        else:
+            print "\tSkipped writing {0}, there was no record".format(locus)
+
+
+def main():
+    args = get_args()
+    alignments = []
+    for ftype in get_file_extensions(args.input_format):
+        alignments.extend(glob.glob(os.path.join(args.input, "*{}".format(ftype))))
+    params = zip([[args.input_format, args.window, args.threshold, args.proportion]] * len(alignments), alignments)
+    sys.stdout.write('Trimming')
+    sys.stdout.flush()
+    if args.multiprocessing:
+        pool = multiprocessing.Pool(multiprocessing.cpu_count() - 1)
+        alignments = pool.map(get_and_trim_alignments, params)
+    else:
+        alignments = map(get_and_trim_alignments, params)
+    write_alignments_to_outdir(args.output, alignments, args.output_format)
+
+
+if __name__ == '__main__':
+    main()
