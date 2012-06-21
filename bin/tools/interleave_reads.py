8f267a54175dad6b5a48e0393a512e8240e27f70
Brant Faircloth <brant.faircloth@gmail.com>
2012-06-21 14:20:16 -0700

    change r1, r2 to read1, read2

--- bin/tools/interleave_reads.py
+++ bin/tools/interleave_reads.py
@@ -12,25 +12,27 @@ into a single "shuffled" or "interleaved" file of reads.
 
 import sys
 import argparse
+from itertools import izip
 from seqtools.sequence import fastq
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Interleave two,"+ \
-            " split, paired-end fastq file into one files"})
+    parser = argparse.ArgumentParser(description="Interleave two split, " + \
+        "paired-end fastq file into one file")
     parser.add_argument("read1", help="The output read1 FASTQ file name")
     parser.add_argument("read2", help="The output read2 FASTQ file name")
     parser.add_argument("output", help="The output fastq file")
     args = parser.parse_args()
     return args
 
+
 def main():
     args = get_args()
     outfile = fastq.FasterFastqWriter(args.output)
-    read1 = fastq.FasterFastqReader(args.r1)
-    read2 = fastq.FasterFastqReader(args.r3)
+    read1 = fastq.FasterFastqReader(args.read1)
+    read2 = fastq.FasterFastqReader(args.read2)
     rc = 0
     sys.stdout.write("Interleaving reads (1 dot = 10,000 pairs): ")
-    for r1,r2 in izip(read1, read2):
+    for r1, r2 in izip(read1, read2):
         assert r1[0].split(" ")[0] == r2[0].split(" ")[0], \
                 "Read FASTQ headers mismatch."
         outfile.write(r1)
@@ -40,5 +42,8 @@ def main():
             sys.stdout.flush()
         rc += 1
     outfile.close()
-    r1.close()
-    r2.close()
+    read1.close()
+    read2.close()
+
+if __name__ == '__main__':
+    main()
