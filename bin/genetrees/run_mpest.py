919706490ef97d15062993f1f33c319f63c2a80b
Brant Faircloth <brant.faircloth@gmail.com>
2012-09-19 11:54:36 -0700

    allow run_mpest to handle tree data from raxml

--- bin/genetrees/run_mpest.py
+++ bin/genetrees/run_mpest.py
@@ -83,6 +83,12 @@ def get_args():
             default=10,
             help="""The number of bootreps to run""",
         )
+    parser.add_argument(
+            "--raxml",
+            action="store_true",
+            default=False,
+            help="""If processing raxml input (versus cloudforest)""",
+        )
     return parser.parse_args()
 
 
@@ -169,9 +175,33 @@ def parse_bootrep_file(fname, root, bootrep_num):
     return genes[0], bootrep_temp_files
 
 
-def get_taxa_for_one_alignment(fname):
+def parse_genetree_file(fname, root, raxml=False):
+    genetree_temp_files = []
+    genetrees = defaultdict(list)
+    for line in open(fname, 'rU'):
+        if raxml:
+            tree_string = line.strip()
+        else:
+            repnum, tree_string = line.strip().split('\t')
+        tree = Tree(tree_string)
+        tree.set_outgroup(root)
+        genetrees[0].append(tree)
+    genes = len(genetrees[0])
+    for repnum, trees in genetrees.iteritems():
+        temp_fd, temp_out = tempfile.mkstemp(prefix='{}-'.format(repnum), suffix='.mpest-bootrep')
+        for tree in trees:
+            os.write(temp_fd, tree.write(format=5) + "\n")
+        os.close(temp_fd)
+        genetree_temp_files.append(temp_out)
+    return genes, genetree_temp_files
+
+
+def get_taxa_for_one_alignment(fname, raxml=False):
     line = open(fname, 'rU').readline()
-    repnum, tree_string = line.strip().split('\t')
+    if raxml:
+        tree_string = line.strip()
+    else:
+        repnum, tree_string = line.strip().split('\t')
     tree_string = tree_string.strip('"')
     tree = Tree(tree_string)
     taxa = tuple(tree.get_leaf_names())
@@ -182,7 +212,24 @@ def main():
     start_time = time.time()
     print 'Started: ', time.strftime("%a %b %d, %Y  %H:%M:%S", time.localtime(start_time))
     args = get_args()
-    if args.bootreps:
+    if not args.bootreps:
+        # get taxa for a given alignment
+        taxa = get_taxa_for_one_alignment(args.control, args.raxml)
+        # parse genetrees from output file
+        genes, temp_genetree_file = parse_genetree_file(args.control, args.root, args.raxml)
+        # create local control file
+        local_control = create_control_file(taxa, genes, temp_genetree_file[0])
+        shutil.move(local_control, os.path.join(os.getcwd(), 'genetree-local-control-file.control'))
+        work = [[taxa, genes, temp_genetree_file[0]]]
+        sys.stdout.write("\nRunning MP-EST against {0} genetrees".format(genes))
+        sys.stdout.flush()
+        tree = map(run_mpest, work)
+        outp = open(args.output, 'w')
+        pdb.set_trace()
+        outp.write(tree[0])
+        outp.write(';\n')
+        outp.close()
+    elif args.bootreps:
         # get taxa for a given alignment
         taxa = get_taxa_for_one_alignment(args.control)
         # parse bootreps from output file
@@ -190,19 +237,19 @@ def main():
         # create local control file
         local_control = create_control_file(taxa, genes, temp_bootrep_files[0])
         shutil.move(local_control, os.path.join(os.getcwd(), 'local-control-file.control'))
-    # cram taxa, gene count, and bootrep files into a list of size num(iterations)
-    work = [[taxa, genes, bootrep] for bootrep in temp_bootrep_files]
-    sys.stdout.write("\nRunning {0} bootreps of {1} iterations".format(args.bootrep_num, args.iterations))
-    sys.stdout.flush()
-    if args.cores == 1:
-        trees = map(run_mpest, work)
-    else:
-        pool = Pool(args.cores)
-        trees = pool.map(run_mpest, work)
-    outp = open(args.output, 'w')
-    outp.write(';\n'.join(trees))
-    outp.write(';')
-    outp.close()
+        # cram taxa, gene count, and bootrep files into a list of size num(iterations)
+        work = [[taxa, genes, bootrep] for bootrep in temp_bootrep_files]
+        sys.stdout.write("\nRunning {0} bootreps of {1} iterations".format(args.bootrep_num, args.iterations))
+        sys.stdout.flush()
+        if args.cores == 1:
+            trees = map(run_mpest, work)
+        else:
+            pool = Pool(args.cores)
+            trees = pool.map(run_mpest, work)
+        outp = open(args.output, 'w')
+        outp.write(';\n'.join(trees))
+        outp.write(';')
+        outp.close()
     end_time = time.time()
     print 'Ended: ', time.strftime("%a %b %d, %Y  %H:%M:%S", time.localtime(end_time))
     print 'Time for execution: ', (end_time - start_time) / 60, 'minutes'
