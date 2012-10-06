3332058db52be310b4f2728b520dc944f94a5604
Brant Faircloth <brant.faircloth@gmail.com>
2012-10-06 14:12:33 -0700

    count characters and total length
    previously, we only counted total length.  now count alignment
    characters as well as total length.  also count any ambiguous
    bases.

--- bin/align/get_align_summary_data.py
+++ bin/align/get_align_summary_data.py
@@ -13,6 +13,7 @@ of alignments having more than "--percent" of "--taxa" taxa.
 
 
 import os
+import re
 import sys
 import glob
 import numpy
@@ -22,7 +23,7 @@ from Bio import AlignIO
 from collections import Counter, defaultdict
 from phyluce.helpers import is_dir, FullPaths, get_file_extensions
 
-#import pdb
+import pdb
 
 
 def get_args():
@@ -58,20 +59,26 @@ def get_files(input_dir, input_format):
 
 
 def pretty_printer(result):
-    print "{:<25}{:<20}".format(result[0], result[1])
+    print "{:<65}{:<20}".format(result[0], result[1])
 
 
-def compute_lengths(lengths):
+def compute_lengths(lengths, characters, ambiguities):
     print "\nLengths\n-----"
     l = numpy.array(lengths)
     sm = ["Total length(aln)", sum(l)]
-    avg = ["Average length(aln)", numpy.mean(l)]
+    avg = ["Mean length(aln)", numpy.mean(l)]
     ci = ["95 CI length(aln)", \
             1.96 * (numpy.std(l, ddof=1) / numpy.sqrt(len(l)))
             ]
     mn = ["Minimum length(aln)", min(l)]
     mx = ["Maximum length(aln)", max(l)]
-    for result in [sm, avg, ci, mn, mx]:
+    char = ["Total characters(aln)", sum(characters)]
+    avg_amb = [numpy.mean(v) for k, v in ambiguities.iteritems()]
+    min_amb = ["Minimum avg. ambiguities(aln)", min(avg_amb)]
+    max_amb = ["Maximum avg. ambiguities(aln)", max(avg_amb)]
+    min_percent_amb = ["Percent avg. ambiguities(aln) as fxn of mean(length)", min(avg_amb) / numpy.mean(l) * 100]
+    max_percent_amb = ["Percent avg. ambiguities(aln) as fxn of mean(length)", max(avg_amb) / numpy.mean(l) * 100]
+    for result in [sm, char, avg, ci, mn, mx, min_amb, max_amb, min_percent_amb, max_percent_amb]:
         pretty_printer(result)
 
 
@@ -109,16 +116,27 @@ def main():
     counts = []
     lengths = []
     trimmed = []
+    characters = []
     bases = defaultdict(list)
+    ambiguities = defaultdict(list)
+    repl = re.compile('\?|n|N|-')
     for f in files:
         try:
             aln = AlignIO.read(f, args.input_format)
             l = aln.get_alignment_length()
             lengths.append(l)
+            name = os.path.basename(f)
             if l < 100:
-                print "{0} is < 100 bp long".format(os.path.basename(f))
-            for col in xrange(len(aln[0])):
-                for base in aln[:, col]:
+                print "{0} is < 100 bp long".format(name)
+            for col_num in xrange(len(aln[0])):
+                col = aln[:, col_num]
+                # get characters
+                informative = re.sub(repl, '', col)
+                col_count = Counter(informative)
+                if len(informative) > 0:
+                    if max(col_count) > 2:
+                        characters.append(1)
+                for base in col:
                     bases[base.upper()].append(1)
             if args.min_taxa:
                 if len(aln) >= args.min_taxa:
@@ -128,16 +146,16 @@ def main():
             else:
                 counts.append(len(aln))
             for read in aln:
-                #pdb.set_trace()
                 left = len(read.seq) - len(str(read.seq).lstrip('-'))
                 right = len(read.seq) - len(str(read.seq).rstrip('-'))
                 trimmed.append(left + right)
+                ambiguities[read.id].append(read.seq.count('N') + read.seq.count('n'))
         except ValueError, e:
             if e.message == 'No records found in handle':
                 print 'No records found in {0}'.format(os.path.basename(f))
             else:
                 raise ValueError('Something is wrong with alignment {0}'.format(os.path.basename(f)))
-    compute_lengths(lengths)
+    compute_lengths(lengths, characters, ambiguities)
     compute_taxa(counts)
     compute_bases(bases, trimmed)
 
