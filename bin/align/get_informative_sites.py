387a6c526f901d329e9ec7c0552465027e1de02e
Brant Faircloth <brant@faircloth-lab.org>
2013-11-17 14:18:06 -0800

    count the number of actual bases/sites interrogated for differences or informative status

--- bin/align/get_informative_sites.py
+++ bin/align/get_informative_sites.py
@@ -7,7 +7,7 @@ Author: Brant Faircloth
 Created by Brant Faircloth on 07 August 2012 21:08 PDT (-0700)
 Copyright (c) 2012 Brant C. Faircloth. All rights reserved.
 
-Description: 
+Description:
 
 """
 
@@ -83,11 +83,18 @@ def get_differences(count):
     del count['N']
     # remove ?
     del count['?']
+    # remove X
+    del count['X']
     sufficient_sites = len(count)
+    # counted, different = (1,1)
     if sufficient_sites >= 2:
-        return True
+        return (1, 1)
+    # counted, not different = (1,0)
+    elif sufficient_sites >= 1 and count.most_common()[0][1] > 1:
+        return (1, 0)
+    # not counted, not different = (0,0)
     else:
-        return False
+        return (0, 0)
 
 def worker(work):
     args, f = work
@@ -95,18 +102,25 @@ def worker(work):
     name = os.path.basename(f)
     informative_sites = []
     differences = []
+    counted_sites =  []
     for idx in xrange(aln.get_alignment_length()):
-        col = aln[:, idx]
+        col = aln[:, idx].upper()
         count = Counter(col)
         if get_informative_sites(count):
             informative_sites.append(1)
         else:
             informative_sites.append(0)
-        if get_differences(count):
+        diff = get_differences(count)
+        if diff == (1, 1):
+            counted_sites.append(1)
             differences.append(1)
+        elif diff == (1, 0):
+            differences.append(0)
+            counted_sites.append(1)
         else:
             differences.append(0)
-    return (name, aln.get_alignment_length(), sum(informative_sites), sum(differences))
+            counted_sites.append(0)
+    return (name, aln.get_alignment_length(), sum(informative_sites), sum(differences), sum(counted_sites))
 
 
 def main():
@@ -119,14 +133,27 @@ def main():
         results = pool.map(worker, work)
     if args.output:
         outf = open(args.output, 'w')
-        outf.write("locus,length,informative_sites,differences\n")
+        outf.write("locus,length,informative_sites,differences,counted-bases\n")
     else:
-        print "locus\tlength\tinformative_sites\tdifferences"
+        print "locus\tlength\tinformative_sites\tdifferences\tcounted-bases"
+    total_sites = []
+    total_differences = []
+    all_counted_sites = []
     for locus in results:
+        total_sites.append(locus[2])
+        total_differences.append(locus[3])
+        all_counted_sites.append(locus[4])
         if not args.output:
-            print "{0}\t{1}\t{2}\t{3}".format(locus[0], locus[1], locus[2], locus[3])
+            print "{0}\t{1}\t{2}\t{3}\t{4}".format(locus[0], locus[1], locus[2], locus[3], locus[4])
         else:
-            outf.write("{0},{1},{2},{3}\n".format(locus[0], locus[1], locus[2], locus[3]))
+            outf.write("{0},{1},{2},{3},{4}\n".format(locus[0], locus[1], locus[2], locus[3], locus[4]))
+    print "Total sites = {0}; Sites per locus = {1:.2f}; Total differences = {2}; Differences per locus = {3:.2f}; All sites checked for differences = {4}".format(
+        sum(total_sites),
+        sum(total_sites) / float(len(total_sites)),
+        sum(total_differences),
+        sum(total_differences) / float(len(total_differences)),
+        sum(all_counted_sites)
+    )
 
 if __name__ == '__main__':
     main()
