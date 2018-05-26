86b7057dea044a073e77a8d5254ed327c6ef55ae
Brant Faircloth <brant@faircloth-lab.org>
2018-05-26 18:02:51 -0500

    simplify git hash as __version__

--- phyluce/__init__.py
+++ phyluce/__init__.py
@@ -1,26 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 import os
 import subprocess
 
+__version__ = "1.6.2"
+
 # get a dynamic version number, if possible.  if not running from git
 # should default to static version
-cwd = os.getcwd()
 try:
-    location = os.path.split(os.path.abspath(__file__))[0]
-    os.chdir(location)
+    possible_git_dir = os.path.dirname(os.path.split(os.path.abspath(__file__))[0])
+    possible_git = os.path.join(possible_git_dir, ".git")
     cmd = [
         "git",
+        "--git-dir",
+        possible_git,
         "rev-parse",
         "--short",
         "HEAD"
     ]
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout, stderr = proc.communicate()
-    if stdout.startswith("fatal:"):
-        raise IOError("{}".format(stdout.strip()))
-    else:
-        __version__ = "git {}".format(stdout.strip())
-    os.chdir(cwd)
+    __version__ = "git {}".format(stdout.strip())
+
 except:
-    __version__ = "1.5.0"
-    if not os.getcwd == cwd:
-        os.chdir(cwd)
+    pass
