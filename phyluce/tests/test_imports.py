dacb5efd0e9102c7b4bc678413b8b1ff1130225b
Brant Faircloth <brant@faircloth-lab.org>
2018-06-07 16:05:09 -0500

    rename test_sample to test_imports

--- phyluce/tests/test_imports.py
+++ phyluce/tests/test_imports.py
@@ -1,7 +1,7 @@
-# content of test_import.py
-import phyluce
+"""Test various imports from packages"""
 
 
-def test_version():
+def test_phyluce_version():
     """Ensure we can successfully import"""
+    import phyluce
     assert phyluce.__version__.startswith("git")
