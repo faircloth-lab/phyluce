cf8cbf54c0d17b874631ece323922a7c96595fea
Brant Faircloth <brant@faircloth-lab.org>
2020-03-25 13:43:35 -0500

    adding tests and modifications for match_contigs_to_probes python 3

---
+++ phyluce/tests/test_match_contigs_to_probes.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+(c) 2020 Brant Faircloth || http://faircloth-lab.org/
+
+All rights reserved.
+
+This code is distributed under a 3-clause BSD license. Please see
+LICENSE.txt for more information.
+
+Created on 25 March 2020 10:24 CDT (-0500)
+"""
+
+import os
+import csv
+import shutil
+import platform
+import subprocess
+
+#from phyluce.tests.common import get_contig_lengths_and_counts
+
+import pytest
+
+import pdb
+
+@pytest.fixture(scope="module")
+def o_dir(request):
+    directory = os.path.join(request.config.rootdir, "test-observed")
+    os.mkdir(directory)
+
+    def clean():
+        shutil.rmtree(directory)
+
+    request.addfinalizer(clean)
+    return directory
+
+
+@pytest.fixture(scope="module")
+def e_dir(request):
+    directory = os.path.join(request.config.rootdir, "phyluce", "tests", "test-results")
+    return directory
+
+@pytest.fixture(scope="module")
+def bait_pth(request):
+    return os.path.join(request.config.rootdir, "phyluce", "tests", "probes", "uce-5k-probes.fasta")
+
+
+@pytest.fixture(scope="module")
+def contig_pth(request, e_dir):
+    return os.path.join(e_dir, "spades", "contigs")
+
+
+def get_match_count_to_probes_results(pth):
+    with open(pth, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        # skip header
+        next(reader)
+        result = {item[0]:[item[1], item[2], item[3], item[4], item[5]] for item in reader}
+    return result
+
+
+def test_match_contigs_to_probes(o_dir, e_dir, bait_pth, contig_pth, request):
+    out_pth = "{}".format(os.path.join(o_dir, "match_contigs"))
+    csv_pth = "{}".format(os.path.join(o_dir, "match_contigs", "match_contig_results.csv"))
+    program = "bin/assembly/phyluce_assembly_match_contigs_to_probes"
+    cmd =[
+        os.path.join(request.config.rootdir, program),
+        "--probes",
+        bait_pth,
+        "--contigs",
+        contig_pth,
+        "--output",
+        out_pth,
+        "--log-path",
+        o_dir,
+        "--csv",
+        csv_pth
+    ]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    print(stderr)
+    observed_count = get_match_count_to_probes_results(csv_pth)
+    expected_count = get_match_count_to_probes_results(
+        os.path.join(
+            e_dir, "probe-match", "probe_match_results.csv"
+        )
+    )
+    for k in observed_count.keys():
+        assert(observed_count[k] == expected_count[k])
