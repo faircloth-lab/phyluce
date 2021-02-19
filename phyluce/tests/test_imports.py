c3bd68ad6a853d8dd293cdf6ce2b75fcb0a91897
Brant Faircloth <brant@faircloth-lab.org>
2021-02-19 12:16:19 -0600

    update version number check in imports

--- phyluce/tests/test_imports.py
+++ phyluce/tests/test_imports.py
@@ -4,4 +4,5 @@
 def test_phyluce_version():
     """Ensure we can successfully import"""
     import phyluce
-    assert phyluce.__version__.startswith("git")
+
+    assert phyluce.__version__.startswith("2.")
