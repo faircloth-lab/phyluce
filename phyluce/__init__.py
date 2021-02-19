7ca775f6f6706fadcda56cc6c17c6ee32053231e
Brant Faircloth <brant@faircloth-lab.org>
2021-02-19 09:24:15 -0600

    update setup files and version number

--- phyluce/__init__.py
+++ phyluce/__init__.py
@@ -4,17 +4,22 @@
 import os
 import subprocess
 
-__version__ = "1.7.0"
+static_version = "2.0.0"
 
 # get a dynamic version number, if possible.  if not running from git
 # should default to static version
 try:
-    possible_git_dir = os.path.dirname(os.path.split(os.path.abspath(__file__))[0])
+    possible_git_dir = os.path.dirname(
+        os.path.split(os.path.abspath(__file__))[0]
+    )
     possible_git = os.path.join(possible_git_dir, ".git")
     cmd = ["git", "--git-dir", possible_git, "rev-parse", "--short", "HEAD"]
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    proc = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
     stdout, stderr = proc.communicate()
-    __version__ = "git {}".format(stdout.strip().decode("utf-8"))
-
+    __version__ = "{}_r_{}".format(
+        static_version, stdout.strip().decode("utf-8")
+    )
 except:
-    pass
+    __version__ = static_version
--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@
 
 import glob
 from setuptools import setup
+from phyluce import static_version
+
 
 align = ["{}".format(i) for i in glob.glob("bin/align/*")]
 assembly = ["{}".format(i) for i in glob.glob("bin/assembly/*")]
@@ -11,6 +13,7 @@ ncbi = ["{}".format(i) for i in glob.glob("bin/ncbi/*")]
 probes = ["{}".format(i) for i in glob.glob("bin/probes/*")]
 snps = ["{}".format(i) for i in glob.glob("bin/snps/*")]
 utilities = ["{}".format(i) for i in glob.glob("bin/utilities/*")]
+workflow = ["{}".format(i) for i in glob.glob("bin/workflow/*")]
 
 scrpt = []
 scrpt.extend(align)
@@ -20,20 +23,24 @@ scrpt.extend(ncbi)
 scrpt.extend(probes)
 scrpt.extend(snps)
 scrpt.extend(utilities)
+scrpt.extend(workflow)
 
 setup(
-    name='phyluce',
-    version='1.6.6',
-    description='software for UCE (and general) phylogenomics',
-    url='https://github.com/faircloth-lab/phyluce',
-    author='Brant C. Faircloth',
-    author_email='borg@faircloth-lab.org',
-    license='BSD',
-    platforms='any',
+    name="phyluce",
+    version=static_version,
+    description="software for UCE (and general) phylogenomics",
+    url="https://github.com/faircloth-lab/phyluce",
+    author="Brant C. Faircloth",
+    author_email="borg@faircloth-lab.org",
+    license="BSD",
+    platforms="any",
     packages=[
-        'phyluce',
+        "phyluce",
     ],
-    data_files=[('config', ['config/phyluce.conf'])],
+    data_files=[
+        ("config", ["config/phyluce.conf"]),
+        ("workflows", ["workflows/*"]),
+    ],
     scripts=scrpt,
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -42,7 +49,7 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Operating System :: POSIX",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
-    )
+)
