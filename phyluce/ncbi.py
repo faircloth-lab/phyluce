9eeddf46362f1ea7cc562136388f5e70317612ff
Brant Faircloth <brant@faircloth-lab.org>
2021-02-20 12:08:26 -0600

    fix some formatting errors identified by black

--- phyluce/ncbi.py
+++ phyluce/ncbi.py
@@ -13,6 +13,7 @@ Created on 09 June 2014 15:14 PDT (-0700)
 
 import os
 
+
 def get_excludes(conf, sec):
     if conf.has_section(sec):
         excludes = [i[0] for i in conf.items(sec)]
@@ -22,25 +23,25 @@ def get_excludes(conf, sec):
 
 
 def get_metadata(conf):
-    return dict(conf.items('metadata'))
+    return dict(conf.items("metadata"))
 
 
 def get_vouchers(conf):
-    if conf.has_section('vouchers'):
-        return dict(conf.items('vouchers'))
+    if conf.has_section("vouchers"):
+        return dict(conf.items("vouchers"))
     else:
         return None
 
 
 def get_remaps(conf):
-    if conf.has_section('remap'):
-        return {i[0].replace(' ', '_'): i[1] for i in conf.items('remap')}
+    if conf.has_section("remap"):
+        return {i[0].replace(" ", "_"): i[1] for i in conf.items("remap")}
     else:
         return None
 
 
 def get_species_name_from_file(infile, remap):
-    sp = os.path.basename(infile).split('.')[0].replace('-', '_')
+    sp = os.path.basename(infile).split(".")[0].replace("-", "_")
     return get_species_name(sp, remap)
 
 
@@ -50,31 +51,33 @@ def get_species_name(sp, remap):
         sp = remap[sp]
     else:
         oldname = sp
-    species = sp.replace('_', ' ').capitalize()
-    partial = species.split(' ')[0].lower()[:3]
+    species = sp.replace("_", " ").capitalize()
+    partial = species.split(" ")[0].lower()[:3]
     return sp, species, partial, oldname
 
 
 def get_node_name(read):
     # check for header match, if match get locus name for header
     nn_split = read.identifier.split("_")[:2]
-    nn = "{}_{}".format(nn_split[0].strip('>').lower(), nn_split[1].lower())
+    nn = "{}_{}".format(nn_split[0].strip(">").lower(), nn_split[1].lower())
     return nn
 
 
 def get_new_identifier(species, uce, partial, counter, metadata, vouchers):
     title = "{0} ultra-conserved element locus {1}".format(species, uce)
-    note = metadata['note'].format(uce)
+    note = metadata["note"].format(uce)
     metadata = {
-                "counter":counter,
-                "partial":partial,
-                "title":title,
-                "organism":"{0}".format(species),
-                "moltype":"{0}".format(metadata['moltype']),
-                "location":"{0}".format(metadata['location']),
-                "note":note,
-                "specimen_voucher":"{0}".format(vouchers[species.lower()])
-            }
-    #pdb.set_trace()
-    new_identifier = "{counter}{partial} [organism={organism}] [moltype={moltype}] [location={location}] [note={note}] [specimen-voucher={specimen_voucher}] {title}".format(**metadata)
+        "counter": counter,
+        "partial": partial,
+        "title": title,
+        "organism": "{0}".format(species),
+        "moltype": "{0}".format(metadata["moltype"]),
+        "location": "{0}".format(metadata["location"]),
+        "note": note,
+        "specimen_voucher": "{0}".format(vouchers[species.lower()]),
+    }
+    # pdb.set_trace()
+    new_identifier = "{counter}{partial} [organism={organism}] [moltype={moltype}] [location={location}] [note={note}] [specimen-voucher={specimen_voucher}] {title}".format(
+        **metadata
+    )
     return new_identifier
