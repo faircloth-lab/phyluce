90823c8b082898c74e02af4aadd524f6f4944b76
Brant Faircloth <brant@faircloth-lab.org>
2024-11-06 11:32:29 -0600

    fix sphinx import

--- docs/conf.py
+++ docs/conf.py
@@ -13,12 +13,12 @@
 
 import sys
 import os
-from phyluce import __version__
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../phyluce'))
+sys.path.insert(0, os.path.abspath('../'))
+from phyluce import __version__
 
 # -- General configuration -----------------------------------------------------
 
