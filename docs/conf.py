71791b3eb16b0003c4c58b30370cd3f27244dfa5
Brant Faircloth <brant@faircloth-lab.org>
2024-11-06 11:36:47 -0600

    update copyright date

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = "index"
 
 # General information about the project.
 project = u"phyluce"
-copyright = u"2012-2023, Brant C. Faircloth"
+copyright = u"2012-2024, Brant C. Faircloth"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
